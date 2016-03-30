fe91384734d3cb03804680e4ecdab2b349566449
P1509142 <P1509142@B710L1109.univ-lyon1.fr>
2016-03-30 17:16:55 +0200

    Merge origin/master
    Conflicts:
    	src/view/Fenetre.java

--- src/view/Fenetre.java
+++ src/view/Fenetre.java
@@ -12,8 +12,12 @@ package view;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
  
@@ -21,9 +25,20 @@ public class Fenetre extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Label lbl = new Label("011");
+        grid.add(lbl,0,1);
+        Button btn = new Button("Start");
+        grid.add(btn,1,1);
+        Label lbl1 = new Label("014");
+        grid.add(lbl1,2,1);
+        
+        /*btn.setOnAction(new EventHandler<ActionEvent>() {
  
             @Override
             public void handle(ActionEvent event) {
@@ -32,15 +47,15 @@ public class Fenetre extends Application {
         });
         
         StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        root.getChildren().add(btn);*/
 
- Scene scene = new Scene(root, 300, 250);
+        Scene scene = new Scene(grid, 300, 250);
 
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Démineur");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
- public void demarrer() {
+    public void demarrer() {
         launch();
     }
 }
