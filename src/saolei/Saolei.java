243c7421046249891dc99f00da52aa623a13899b
P1509142 <P1509142@B710L1109.univ-lyon1.fr>
2016-03-30 16:43:02 +0200

    main corri

--- src/saolei/Saolei.java
+++ src/saolei/Saolei.java
@@ -7,31 +7,12 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import view.Fenetre;
  
-public class Saolei extends Application {
-    
-    @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();  
-        btn.setText("Say 'Hello World!!' ");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-
-        Scene scene = new Scene(root, 300, 250);
-
-               primaryStage.setTitle("Hello World!");
-               primaryStage.setScene(scene);
-               primaryStage.show();
-           }
-        public static void main(String[] args) {
-               launch(args);
-           }
+public class Saolei{
+     public static void main(String[] args) {
+         Fenetre f = new Fenetre();
+         f.demarrer();
+         
+     }
 }
