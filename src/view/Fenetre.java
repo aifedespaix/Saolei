1e3c8bc68b90273e9a06459d365c6ebb47e674bd
sodom <015585565@gmx.fr>
2016-04-06 14:36:01 +0200

    Mise en place interface Web

--- src/view/Fenetre.java
+++ src/view/Fenetre.java
@@ -9,53 +9,48 @@ package view;
  *
  * @author P1509142
  */
+import java.io.File;
 import javafx.application.Application;
+import javafx.application.ConditionalFeature;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
  
 public class Fenetre extends Application {
+    String  title           = "Saolei - Demineur";
+    int     defaultSizeWinX = 600;
+    int     defaultSizeWinY = 400;
+    Color   bgColor         = new Color(1, 0.4, 0.02, 1);
     
     @Override
     public void start(Stage primaryStage) {
-        GridPane grid = new GridPane();
-        grid.setAlignment(Pos.CENTER);
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(25, 25, 25, 25));
-
-        Label lbl = new Label("011");
-        grid.add(lbl,0,1);
-        Button btn = new Button("Start");
-        grid.add(btn,1,1);
-        Label lbl1 = new Label("014");
-        grid.add(lbl1,2,1);
+        final WebView webView = new WebView();
         
-        /*btn.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+        final String pageURI= new File("html/index.html").toURI().toString(); 
+        webView.getEngine().load(pageURI);
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);*/
+        final Scene scene = new Scene(webView, defaultSizeWinX, defaultSizeWinY); 
+        primaryStage.setTitle(title); 
+        primaryStage.setScene(scene); 
+        
+        primaryStage.show(); 
 
-        Scene scene = new Scene(grid, 300, 250);
-
-        primaryStage.setTitle("Démineur");
-        primaryStage.setScene(scene);
-        primaryStage.show();
     }
+    
     public void demarrer() {
-        launch();
+        if(Platform.isSupported(ConditionalFeature.WEB))
+            launch();
+        else System.out.println("Désolé ça marche pas");
     }
 }
